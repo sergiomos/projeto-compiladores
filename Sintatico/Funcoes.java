0a4574ad1e4f0e10af428c05ac02503a6f987bd9
Sergio Martins <sergio.martins@nubank.com.br>
2025-05-15 15:45:46 -0300

    finalizando funcao

--- Sintatico/Funcoes.java
+++ Sintatico/Funcoes.java
@@ -11,6 +11,22 @@ public class Funcoes {
     return parser.matcher.matchT("FUNCAO", "fn ")
         && parser.elementos.id()
         && parser.matcher.matchL("(", "(")
-        && parser.matcher.matchL(")", ")");
+        && parametros()
+        && parser.matcher.matchL(")", ")")
+        && parser.matcher.matchL("{", "{\n")
+        && parser.programa.bloco()
+        && parser.matcher.matchL("}", "}");
+  }
+
+  private boolean parametros() {
+    return parametro()
+        || (parametro()
+            && parser.matcher.matchL(",", ", ") && parametros());
+
+  }
+
+  private boolean parametro() {
+    return (parser.elementos.tipo() && parser.elementos.id())
+        || true;
   }
 }
